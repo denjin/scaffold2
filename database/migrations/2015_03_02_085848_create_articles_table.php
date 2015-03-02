e469f43ca1aa50e51ab132b0db81a76891b1d185
denjin <chris_luffingham@hotmail.com>
2015-03-02 09:11:03 +0000

    Initial Commit

---
+++ database/migrations/2015_03_02_085848_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('articles', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('title', 256);
+			$table->text('body');
+			$table->integer('user_id');
+			$table->timestamps();
+			$table->timestamp('published_at');
+			$table->string('slug', 256);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('articles');
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Scaffold
+
+A rapid deployment base for small to medium sized web development projects.
+
+Will contain all the key requirements for a basic commercial website including lightweight content management system, interactive gallery, blog with comments and typical page structure.
+
+Made with Laravel 5, built on Twitter Bootstrap for extensibility.
